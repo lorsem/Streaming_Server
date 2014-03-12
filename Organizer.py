e073d366c6572836c83f40830d80e2b3f864edad
lorsem <lorsem@hotmail.it>
2014-03-12 23:56:12 +0100

    Indexer seems to work recursively

--- Organizer.py
+++ Organizer.py
@@ -4,42 +4,64 @@ import os
 import sys
 import shutil
 
+
+def CheckIfDirIsOK(TheDir):
+    if TheDir.startswith('.') or TheDir.startswith('__'):
+        return False
+    return True
+
+def NestedDir(ThePath):
+    for name in os.listdir(ThePath):
+        if CheckIfDirIsOK(name):
+            if os.path.isdir(ThePath + '/' + name):
+                return True
+  
+    return False
+    
+
 def CreateIndex(RootDir):#!!!!!!STUB
-    indexfile = open('index.html', 'w')
+    
+    indexfile = open(RootDir + '/' + 'index.html', 'w')
     indexfile.write('''
         <!DOCTYPE html>
         <html>
         <body>
         ''')
     for TheDirectory in os.listdir(RootDir): #for every file in directory
-        if os.path.isdir(RootDir + '/' + TheDirectory): #if the 'file' is a directory
-            if not TheDirectory.startswith('.'): #and the directory is not hidden
-                if not os.path.isdir(spam for spam in os.listdir('')):                        #and there is no directory in the directory:
+        if os.path.isdir(RootDir + '/' + TheDirectory):#if the 'file' is a directory
+            if CheckIfDirIsOK(TheDirectory): #and the directory is not hidden
+                if not NestedDir(RootDir + '/' + TheDirectory):  #and there is no directory in the directory:
                     formatDir = TheDirectory.replace(' ', '%20')
                     formatDir = formatDir.replace('[', '%5B')
                     formatDir = formatDir.replace(']', '%5D')
                     #Is it ok <a href=".//*TheDirectory, etcetera..*
-                    indexfile.write('''<a href="{0}">{1}</a>
-                <br>'''.format('./' + TheDirectory + '/' + 'content.html', TheDirectory))
-                else:
-                    
-                    CreateIndex()
+                    indexfile.write('''<a href="{0}">{1}</a><br>
+                                    '''.format('./' + TheDirectory + '/' + 'content.html', TheDirectory))
+                if NestedDir(RootDir + '/' + TheDirectory):
+                    indexfile.write('''<a href="{0}">{1}</a><br>
+                                    '''.format('./' + TheDirectory + '/' + 'index.html', 'Multi-file' + TheDirectory))
+                    CreateIndex(RootDir + '/' + TheDirectory)
+        
+    indexfile.write('''
+                  </body>
+                  </html>
+                  ''')
+    indexfile.close()               
+    
 
 def AddContentHtml():
     pass
 
-def LayDownFiles(RootDir): #Works Recursively
+def LayDownFiles(RootDir): #Works Recursively - Tests are ok atm
     for name in os.listdir(RootDir):
-        if not name.startswith('.') and not name.startswith('__'):
+        if CheckIfDirIsOK(name):
             if os.path.isfile(RootDir + '/' + name) and not os.path.isfile(RootDir + '/.IsLaid.chk'): #If it is a file and has not been laid yet
                 #         ./test
                 
                 vidformat = name.split('.')
                 vidformat = vidformat[len(vidformat)-1]
                 if vidformat == 'html':
-                    print('TheIF')
                     break
-                print('\nFORMAT:', vidformat, '\n',  repr(vidformat),  '\n\n')
                 newpath = RootDir + "/" + name[:-len(vidformat)-1]
                 os.makedirs(newpath)
                 open(newpath + '/.IsLaid.chk', 'w').close()
@@ -57,16 +79,15 @@ def LayDownFiles(RootDir): #Works Recursively
 
 
 print('Starting to lay out files in current directory...')
-LayDownFiles(os.getcwd())
+CWD = os.getcwd()
+LayDownFiles(CWD)
 print('DONE')
-'''
-AddContentHtml()
 
-print('Creating indexes')
-'''
-    
+#AddContentHtml()
 
-
+print('Creating indexes...')
+CreateIndex(CWD)
+print('DONE')
 print('Bye')
 
 
